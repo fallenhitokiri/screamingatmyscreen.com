6c3240c6de9e8a3a764b93bd8bc265e5ff6730fb
Timo Zimmermann <timo@screamingatmyscreen.com>
2013-05-17 16:56:58 +0200

    2013-05-17 16:56:51.906978

---
+++ template/js/capam.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    var host = "http://capam.herokuapp.com";
+    var id_length = 128;
+
+    function randomID() {
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var randomstring = '';
+        for (var i = 0; i < id_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        return randomstring;
+    }
+
+    var setUserID = function() {
+        var id = randomID();
+        document.cookie = "id=" + id + ";max-age=2592000;";
+        return id;
+    };
+
+    var getUserID = function() {
+        cookie = document.cookie;
+        console.log(cookie);
+        console.log(cookie.indexOf('id='));
+
+        if (cookie.length > 0) {
+            if (cookie.indexOf('id=') !== -1) {
+                var ind = cookie.indexOf('id=') + 3;
+                return cookie.substring(ind, id_length);
+            } else {
+                return setUserID();
+            }
+        } else {
+            return setUserID();
+        }
+    };
+
+    $.ajax({
+        data: {
+            'referrer': document.referrer,
+            'token': token,
+            'userid': getUserID
+        },
+        type: 'POST',
+        url: host
+    });
+});
